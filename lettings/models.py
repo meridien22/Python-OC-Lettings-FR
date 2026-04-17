e43992676a503ead48a9d554ef56f82e2bd3b784
meridien22 <meridien.sig@gmail.com>
2026-04-17 07:38:28 +0100

    pluralisation

--- lettings/models.py
+++ lettings/models.py
@@ -3,6 +3,11 @@ from django.core.validators import MaxValueValidator, MinLengthValidator
 
 
 class Address(models.Model):
+
+    class Meta:
+        verbose_name = "Address"
+        verbose_name_plural = "Address"
+
     number = models.PositiveIntegerField(validators=[MaxValueValidator(9999)])
     street = models.CharField(max_length=64)
     city = models.CharField(max_length=64)
