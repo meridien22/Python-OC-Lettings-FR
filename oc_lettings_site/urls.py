9a73f647ede57d849a57fad72cccae60520ed705
meridien22 <meridien.sig@gmail.com>
2026-04-17 13:41:59 +0100

    404 and 500 error

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler404, handler500
 
 from . import views
 
+handler404 = 'oc_lettings_site.views.error_404_custom'
+handler500 = 'oc_lettings_site.views.error_500_custom'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('error_404', views.error_404, name='error_404'),
+    path('error_500', views.error_500, name='error_500'),
     path('lettings/', include('lettings.urls')),
     path('profiles/', include('profiles.urls')),
     path('admin/', admin.site.urls),
--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 
 
 # Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie quam lobortis
@@ -9,3 +10,16 @@ from django.shortcuts import render
 # Praesent volutpat porttitor magna, non finibus neque cursus id.
 def index(request):
     return render(request, 'index.html')
+
+def error_404(request):
+    raise Http404()
+
+def error_500(request):
+    resultat = 1 / 0
+    return render(request, 'index.html')
+
+def error_404_custom(request, exception):
+    return render(request, '404.html', status=404)
+
+def error_500_custom(request, *args, **kwargs):
+    return render(request, '500.html', status=500)
